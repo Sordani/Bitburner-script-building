bffb68c76d8fc910337b3d594b703459dd57bae1
Sordani <131472756+Sordani@users.noreply.github.com>
2023-04-26 17:34:36 -0700

    Create hack.js
    worker script futureproofed for batching.

---
+++ hack.js
@@ -0,0 +1,6 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	const target = ns.args[0];
+	// ?? checks for null and undefined statments. this syntax will set to 0 if either of those are true.
+	const waitTime = ns.args[1] ?? 0;
+	await ns.hack(target,{additionalMsec:waitTime});
