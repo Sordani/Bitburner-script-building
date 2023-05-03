36a56e4bf9c56e18bc25477821103df56095c198
Sordani <131472756+Sordani@users.noreply.github.com>
2023-05-03 15:58:40 -0700

    Update Protobatch.js
    removed unused functions, updated from HGW to HWGW, the superior ram efficiency model, and updated log loop to include all the time variables so the log is always producing pertinent data. also included logic to count down a start time for grow and hack, then switch to the normal countdown. the spacer is 30 milliseconds, so that won't ever show in the countdown as useable data since the log refreshes once per second at the quickest (1000 milliseconds). this should be the last update to this script, as this will be the basis for the actual batching script i make separately.

--- Protobatch.js
+++ Protobatch.js
@@ -1,65 +1,8 @@
 /** @param {NS} ns */
-// Solve for number of growth threads required to get from money_lo to money_hi
-// base is ns.formulas.hacking.growPercent(serverObject, 1, playerObject, cores)
-function solveGrow(base, money_lo, money_hi) {
-    if (money_lo >= money_hi) { return 0; }
-
-    let threads = 1000;
-    let prev = threads;
-    for (let i = 0; i < 30; ++i) {
-        let factor = money_hi / Math.min(money_lo + threads, money_hi - 1);
-        threads = Math.log(factor) / Math.log(base);
-        if (Math.ceil(threads) == Math.ceil(prev)) { break; }
-        prev = threads;
-    }
-
-    return Math.ceil(Math.max(threads, prev, 0));
-}
-
-/** @param {NS} ns */
 function is_prepped(ns, host) {
     return ns.getServerSecurityLevel(host) <= ns.getServerMinSecurityLevel(host) && ns.getServerMoneyAvailable(host) >= ns.getServerMaxMoney(host);
 }
 
-/** @param {NS} ns */
-//function loop checks if each program that open ports exists.
-function countPrograms(ns) {
-    const fileArray = ["brutessh.exe", "ftpcrack.exe", "relaysmtp.exe", "httpworm.exe", "sqlinject.exe"];
-    let fileCount = 0;
-    for (let i = 0; i < fileArray.length;) {
-        fileCount += ns.fileExists(fileArray[i], 'home');
-        i++;
-    }
-    return fileCount;
-}
-
-/** @param {NS} ns */
-// copied this straight from #early-game pinned messages in the bitburner discord. posted by xsinx#1018
-// Returns a weight that can be used to sort servers by hack desirability
-function Weight(ns, server) {
-    if (!server) return 0;
-    if (server.startsWith('hacknet-node')) return 0;
-    let player = ns.getPlayer();
-    let so = ns.getServer(server);
-    so.hackDifficulty = so.minDifficulty;
-    if (so.requiredHackingSkill > ns.getHackingLevel()) { return 0; }
-    if (ns.getServerNumPortsRequired(server) > countPrograms(ns)) { return 0; }
-    let weight = so.moneyMax / so.minDifficulty;
-    if (ns.fileExists('Formulas.exe')) {
-        weight = so.moneyMax / ns.formulas.hacking.weakenTime(so, player) * ns.formulas.hacking.hackChance(so, player);
-    }
-    else
-        if (so.requiredHackingSkill > ns.getHackingLevel())
-            return 0;
-    return weight;
-}
-
-/** @param {NS} ns */
-function updateTarget(ns) { //function to grab list of servers and assign hackability weight to them. requires Weight function.
-    return getServerList(ns).map((server) => ({ name: server, score: Weight(ns, server) }))
-}
-
-/** @param {NS} ns */
 export async function main(ns) {
     ns.disableLog('ALL');
     ns.tail();
@@ -77,50 +20,85 @@ export async function main(ns) {
     }
 
     while (true) {
-        const hackThreadcount = ns.fileExists('formulas.exe') ? ns.formulas.hacking.hackPercent(server.hostname, player, greed) : Math.floor(ns.hackAnalyzeThreads(target, hackSet));
+        const hThreads = ns.fileExists('formulas.exe') ? ns.formulas.hacking.hackPercent(server, player, greed) : Math.floor(ns.hackAnalyzeThreads(target, hackSet));
         server.moneyAvailable = server.moneyMax * (1 - greed);
-        const growThreadcount = ns.fileExists('formulas.exe') ? ns.formulas.hacking.growThreads(server.hostname, player, server.moneyMax) : Math.ceil(ns.growthAnalyze(server.hostname, (server.moneyMax / server.moneyAvailable)) + 0.001);
-        const weakThreadcount = Math.ceil((hackThreadcount * 0.002) + (growThreadcount * 0.004) / 0.05);
+        const gThreads = ns.fileExists('formulas.exe') ? ns.formulas.hacking.growThreads(server, player, server.moneyMax) : Math.ceil(ns.growthAnalyze(server.hostname, (server.moneyMax / server.moneyAvailable)) + 0.001);
+        const wThreads1 = Math.max(Math.ceil(hThreads * 0.002 / 0.05), 1);
+        const wThreads2 = Math.max(Math.ceil(gThreads * 0.004 / 0.05), 1);
         let wkTime = ns.getWeakenTime(server.hostname);
         let hkTime = ns.getHackTime(server.hostname);
         let gwTime = ns.getGrowTime(server.hostname);
-        let hackPID = ns.exec("hack.js", "home", hackThreadcount, target);
+        const spacer = 30;
+        let hkDelay = wkTime - spacer - hkTime;
+        let wk1Delay = 0;
+        let gwDelay = wkTime + spacer - gwTime;
+        let wk2Delay = spacer * 2;
+        let hackPID = ns.exec("hack.js", "home", hThreads, target, hkDelay);
         ns.print(hackPID);
         if (hackPID == 0) {
             break;
         }
-
-        let growPID = ns.exec("grow.js", "home", growThreadcount, target);
+        let weakPID1 = ns.exec("weak.js", "home", wThreads1, target, wk1Delay);
+        ns.print(weakPID1);
+        if (weakPID1 == 0) {
+            break;
+        }
+        let growPID = ns.exec("grow.js", "home", gThreads, target, gwDelay);
         ns.print(growPID);
         if (growPID == 0) {
             break;
         }
-
-        let weakPID = ns.exec("weak.js", "home", weakThreadcount + 10, target);
-        ns.print(weakPID);
-        if (weakPID == 0) {
+        let weakPID2 = ns.exec("weak.js", "home", wThreads2, target, wk2Delay);
+        ns.print(weakPID2);
+        if (weakPID2 == 0) {
             break;
         }
-        const duration = wkTime + 100;
-        const expectedEnd = performance.now() + duration;
-        let timeRemaining = expectedEnd - performance.now();
-        while (timeRemaining > 0) {
-            timeRemaining = expectedEnd - performance.now();
+
+        let expectedEndh = performance.now() + hkTime + hkDelay;
+        let expectedStarth = performance.now() + hkDelay;
+        let expectedEndw1 = performance.now() + wkTime + wk1Delay;
+        let expectedEndg = performance.now() + gwTime + gwDelay;
+        let expectedStartg = performance.now() + gwDelay;
+        let expectedEndw2 = performance.now() + wkTime + wk2Delay;
+        let timeRemainingh = expectedEndh - performance.now();
+        let timeToStarth = expectedStarth - performance.now();
+        let timeRemainingw1 = expectedEndw1 - performance.now();
+        let timeRemainingg = expectedEndg - performance.now();
+        let timeToStartg = expectedStartg - performance.now();
+        let timeRemainingw2 = expectedEndw2 + wk2Delay - performance.now();
+        while (timeRemainingw2 > 0) {
+            timeRemainingh = expectedEndh - performance.now();
+            timeRemainingw1 = expectedEndw1 - performance.now();
+            timeRemainingg = expectedEndg - performance.now();
+            timeRemainingw2 = expectedEndw2 + wk2Delay - performance.now();
+            timeToStarth = expectedStarth - performance.now();
+            timeToStartg = expectedStartg - performance.now();
             ns.clearLog();
+            ns.print('  Item       :  Hack, 1st Weaken, Grow, 2nd Weaken')
+            ns.print('HWGW threads : ' + hThreads + ', ' + wThreads1 + ', ' + gThreads + ', ' + wThreads2);
+            ns.print('HWGW times   : ' + ns.formatNumber(hkTime, 0, 1000000) + ', ' + ns.formatNumber(wkTime, 0, 1000000) + ', ' + ns.formatNumber(gwTime, 0, 1000000) + ', ' + ns.formatNumber(wkTime, 0, 1000000));
+            ns.print('HWGW delays  : ' + ns.formatNumber(hkDelay, 0, 1000000) + ', ' + ns.formatNumber(wk1Delay, 0, 1000000) + ', ' + ns.formatNumber(gwDelay, 0, 1000000) + ', ' + ns.formatNumber(wk2Delay, 0, 1000000));
+            ns.print('HWGW ends    : ' + ns.formatNumber(hkDelay + hkTime, 0, 1000000) + ', ' + ns.formatNumber(wkTime + wk1Delay, 0, 1000000) + ', ' + ns.formatNumber(gwTime + gwDelay, 0, 1000000) + ', ' + ns.formatNumber(wkTime + wk2Delay, 0, 1000000));
             ns.print('hackPID: ' + hackPID);
-            ns.print('hack thread count: ' + hackThreadcount);
-            ns.print('hackTime: ' + ns.tFormat(hkTime));
+            if (timeToStarth > 0) {
+                ns.print('hack will initiate process in: ' + ns.tFormat(timeToStarth));
+            } else {
+                ns.print('hack has started and will commit/end in: ' + ns.tFormat(timeRemainingh));
+            }
+            ns.print('weakPID1: ' + weakPID1);
+            ns.print('1st weaken will commit/end in: ' + ns.tFormat(timeRemainingw1));
             ns.print('growPID: ' + growPID);
-            ns.print('grow thread count: ' + growThreadcount);
-            ns.print('growTime: ' + ns.tFormat(gwTime));
-            ns.print('weakPID: ' + weakPID);
-            ns.print('weaken thread count: ' + weakThreadcount);
-            ns.print('weakTime: ' + ns.tFormat(wkTime));
-            ns.print('duration: ' + ns.tFormat(duration));
-            ns.print('timeRemaining: ' + ns.tFormat(timeRemaining));
+            if (timeToStartg > 0) {
+                ns.print('grow will initiate process in: ' + ns.tFormat(timeToStartg));
+            } else {
+                ns.print('grow has started and will commit/end in ' + ns.tFormat(timeRemainingg));
+            }
+            ns.print('weakPID2: ' + weakPID2);
+            ns.print('2nd weaken will commit/end in: ' + ns.tFormat(timeRemainingw2));
             await ns.sleep(100);
         }
 
         ns.print('Countdown Complete.');
+
     }
 } //make sure grow.js, weak.js, and hack.js exist
