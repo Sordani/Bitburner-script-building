67ed0f173d38e54593cf52e4a8922afdd149015e
Sordani <131472756+Sordani@users.noreply.github.com>
2023-05-04 17:24:44 -0700

    Update brain.js
    updated to make use of script library (slibs.js). homeAttack commented out due to use of home ram for protobatcher.

--- brain.js
+++ brain.js
@@ -1,56 +1,10 @@
+//importing functions from slibs.js function library for cleanliness.
+import { countPrograms, canHack, getServerList, Weight, updateTarget, getBestTarget, breakingAndEntering } from "slibs.js";
+
 /**
  * @param {NS} ns
  * @returns {[]string}
  */
-
-//function loop checks if each program that open ports exists.
-function countPrograms(ns) {
-	const fileArray = ["brutessh.exe", "ftpcrack.exe", "relaysmtp.exe", "httpworm.exe", "sqlinject.exe"];
-	let fileCount = 0;
-	for (let i = 0; i < fileArray.length;) {
-		fileCount += ns.fileExists(fileArray[i], 'home');
-		i++;
-	}
-	return fileCount;
-}
-
-//function called canHack that tests the ports required to hack and the hacking level required to access.
-//returns false if either tests aren't passed, returns true otherwise.
-function canHack(ns, server) {
-	if (ns.getServerRequiredHackingLevel(server) > ns.getHackingLevel()) return false;
-	if (ns.getServerNumPortsRequired(server) > countPrograms(ns)) return false;
-	return true;
-}
-
-//function to open all the ports on the parameter server.
-async function breakingAndEntering(ns, server, portScript) {
-	ns.print('breakingAndEntering called');
-	ns.print('breakingAndEntering thinks the server is: ' + server);
-	ns.exec(portScript, 'home', 1, server);
-	await ns.sleep(100);
-	ns.print('breakingAndEntering applied to ' + server);
-}
-
-
-// copied this straight from #early-game pinned messages in the bitburner discord. posted by xsinx#1018
-// Returns a weight that can be used to sort servers by hack desirability
-function Weight(ns, server) {
-	if (!server) return 0;
-	if (server.startsWith('hacknet-node')) return 0;
-	let player = ns.getPlayer();
-	let so = ns.getServer(server);
-	so.hackDifficulty = so.minDifficulty;
-	if (so.requiredHackingSkill > ns.getHackingLevel()) { return 0; }
-	if (ns.getServerNumPortsRequired(server) > countPrograms(ns)) { return 0; }
-	let weight = so.moneyMax / so.minDifficulty;
-	if (ns.fileExists('Formulas.exe')) {
-		weight = so.moneyMax / ns.formulas.hacking.weakenTime(so, player) * ns.formulas.hacking.hackChance(so, player);
-	}
-	else
-		if (so.requiredHackingSkill > ns.getHackingLevel())
-			return 0;
-	return weight;
-}
 
 //kills the running script and runs new script on parameter server at parameter target.
 function employ(ns, server, paramTarget, virus, virusRam) {
@@ -60,41 +14,13 @@ function employ(ns, server, paramTarget, virus, virusRam) {
 	ns.exec(virus, server, maxThreads, paramTarget);
 }
 
-
-function getServerList(ns) {
-	let found = [];
-	let unscanned = ['home'];
-
-	while (unscanned.length > 0) {
-		let server = unscanned.pop();
-		let neighboors = ns.scan(server);
-		for (let i = 0; i < neighboors.length; i++) {
-			if (found.includes(neighboors[i])) continue;
-			found.push(neighboors[i]);
-			unscanned.push(neighboors[i]);
-		}
-
-	}
-	return found;
-}
-
-//function to return a 2d array containing the results of getServerList(); in one column and Weight(); in another
-function updateTarget(ns) {
-	return getServerList(ns).map((server) => ({ name: server, score: Weight(ns, server) }))
-}
-
-//function that requires the results of updateTarget() and parses the information to find the highest weighted server name.
-function getBestTarget(ns, serverList) {
-	return serverList.sort((a, b) => b.score - a.score)[0]
-}
-
 //Function to call the results of getServerList() and go item by item through the array
 //to gain root access and utilize the ram if any is available.
-async function attack(ns, server, virus, virusRam, portScript) {
+async function attack(ns, server, virus, virusRam) {
 	let curTar = getServerList(ns);
 	for (let i = 0; i < curTar.length; i++) {
 		if (canHack(ns, curTar[i])) {
-			await breakingAndEntering(ns, curTar[i], portScript);
+			await breakingAndEntering(ns, curTar[i]);
 			if (ns.getServerMaxRam(curTar[i]) > virusRam) {
 				employ(ns, curTar[i], server, virus, virusRam);
 			}
@@ -142,16 +68,14 @@ export async function main(ns) {
 	ns.tail();
 	//moneymaking script. default hack/grow/weaken loop or 'early-hack-template.js'
 	const virus = 'early-hack-template.js';
-	//script that contains logic to run all port opening programs and nuke.exe.
-	const portScript = 'portopener.js'
 	const virusRam = ns.getScriptRam(virus);
 	//arbitrary amount of ram to keep open on home computer. required for calling other scripts
 	const reserveRam = 4;
 	let serverListWeights = updateTarget(ns);
 	let target = getBestTarget(ns, serverListWeights).name;
 	ns.print('Target is now: ' + target);
-	await breakingAndEntering(ns, target, portScript);
-	await attack(ns, target, virus, virusRam, portScript)
+	await breakingAndEntering(ns, target);
+	await attack(ns, target, virus, virusRam)
 	commandPurchasedServers(ns, target, virus, virusRam)
 	//homeAttack(ns, target, virus, virusRam, reserveRam);
 
@@ -165,8 +89,8 @@ export async function main(ns) {
 		if (curTar == savedTar) continue;
 		curTar = savedTar;
 		ns.print('Target is now: ' + curTar);
-		await breakingAndEntering(ns, curTar, portScript);
-		await attack(ns, curTar, virus, virusRam, portScript);
+		await breakingAndEntering(ns, curTar);
+		await attack(ns, curTar, virus, virusRam);
 		commandPurchasedServers(ns, curTar, virus, virusRam)
 		//homeAttack(ns, curTar, virus, virusRam, reserveRam);
 
