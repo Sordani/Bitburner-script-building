2b1d2d120af6be4b98d14377234edc2065757102
Sordani <131472756+Sordani@users.noreply.github.com>
2023-05-10 12:30:05 -0700

    Update Protobatch.js
    rewrote the whole thing after working on a shotgun batcher.

--- Protobatch.js
+++ Protobatch.js
@@ -1,106 +1,106 @@
-/** @param {NS} ns */
-function is_prepped(ns, host) {
-    return ns.getServerSecurityLevel(host) <= ns.getServerMinSecurityLevel(host) && ns.getServerMoneyAvailable(host) >= ns.getServerMaxMoney(host);
-}
+//importing functions from slibs.js function library for cleanliness.
+import { buildramNetwork, solveGrow, isPrepped, prepTarget, countPrograms, Weight, getServerList, updateTarget, getBestTarget } from "slibs.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
-    ns.disableLog('ALL');
-    ns.tail();
+    
+    
+    let ramNetwork = [];
+    const values = {
+        totalPossibleThreads: 0,
+        optimalTarget: 'n00dles',
+        maxServerRamSize: 0,
+        minServerRamSize: Infinity,
+    }
+    //values.optimalTarget = getBestTarget(ns, updateTarget(ns)); //this gets the best target
+    values.optimalTarget = updateTarget(ns).sort((a, b) => b.score - a.score)[1].name    //this will get the second best target since we have earlyhacktemplate running everywhere else against the best.
+    buildramNetwork(ns, ramNetwork, values);
 
-    const target = ns.args[0];
-    const player = ns.getPlayer();
-    const server = ns.getServer(target);
-    const greed = 0.02; //2% to start and learn
-    const hackSet = server.moneyMax * greed;
+    const jobs = ['weaken2', 'grow', 'weaken1', 'hack'];
 
-    ns.print(server);
-    if (!is_prepped(ns, server.hostname)) {
-        ns.print('Target not prepped (not at minSecurity or MaxMoney). you included code here to stop instead of doing anything about it.');
-        ns.exit();
+    ns.disableLog("ALL");
+	ns.tail();
+	const dataPort = ns.getPortHandle(ns.pid);
+	dataPort.clear();
+
+	// Spawns a special helper script for centralizing worker logs. See the script itself for details.
+	if (ns.isRunning("batchlog.js", "home")) ns.scriptKill("batchlog.js", "home");
+	const logPort = ns.exec("batchlog.js", "home");
+	ns.atExit(() => ns.kill(logPort));  // Kill the logger when the controller ends.
+	await ns.sleep(3000); // This is just to give the helper a moment to initialize.
+
+
+    if (!isPrepped(ns, values.optimalTarget)) {
+        await prepTarget(ns, values.optimalTarget);
+        ramNetwork = [];
+        values.minServerRamSize = Infinity;
+        values.maxServerRamSize = 0;
+        values.totalThreads = 0
+        buildramNetwork(ns, ramNetwork, values);
     }
 
+    const player = ns.getPlayer();
+    let server = ns.getServer(values.optimalTarget);
+	let greed = 0.20
+    let hackSet = server.moneyMax * greed;
+
+
+
     while (true) {
-        const hThreads = ns.fileExists('formulas.exe') ? ns.formulas.hacking.hackPercent(server, player, greed) : Math.floor(ns.hackAnalyzeThreads(target, hackSet));
+        const hThreads = ns.fileExists('formulas.exe') ? Math.ceil(greed / (ns.formulas.hacking.hackPercent(server, player))) : Math.max(Math.floor(ns.hackAnalyzeThreads(values.optimalTarget, hackSet)), 1);
         server.moneyAvailable = server.moneyMax * (1 - greed);
         const gThreads = ns.fileExists('formulas.exe') ? ns.formulas.hacking.growThreads(server, player, server.moneyMax) : Math.ceil(ns.growthAnalyze(server.hostname, (server.moneyMax / server.moneyAvailable)) + 0.001);
         const wThreads1 = Math.max(Math.ceil(hThreads * 0.002 / 0.05), 1);
         const wThreads2 = Math.max(Math.ceil(gThreads * 0.004 / 0.05), 1);
-        let wkTime = ns.getWeakenTime(server.hostname);
-        let hkTime = ns.getHackTime(server.hostname);
-        let gwTime = ns.getGrowTime(server.hostname);
+        //calculate weaken time, then hack time is one fourth weaken time, and growtime is 3.2 times hacktime.
+        const wkTime = ns.getWeakenTime(server.hostname);
+        const hkTime = wkTime / 4;
+        const gwTime = hkTime * 3.2;
+        //spacer is the tolerance for desync we're allowing. testing for as small as possible, starting at 30 ms
         const spacer = 30;
-        let hkDelay = wkTime - spacer - hkTime;
-        let wk1Delay = 0;
-        let gwDelay = wkTime + spacer - gwTime;
-        let wk2Delay = spacer * 2;
-        let hackPID = ns.exec("hack.js", "home", hThreads, target, hkDelay);
-        ns.print(hackPID);
-        if (hackPID == 0) {
-            break;
-        }
-        let weakPID1 = ns.exec("weak.js", "home", wThreads1, target, wk1Delay);
-        ns.print(weakPID1);
-        if (weak1PID == 0) {
-            break;
-        }
-        let growPID = ns.exec("grow.js", "home", gThreads, target, gwDelay);
-        ns.print(growPID);
-        if (growPID == 0) {
-            break;
-        }
-        let weakPID2 = ns.exec("weak.js", "home", wThreads2, target, wk2Delay);
-        ns.print(weakPID2);
-        if (weak2PID == 0) {
-            break;
+        const hkdelay = wkTime - hkTime + -5 + spacer;
+        const wkdelay1 = 0 + spacer;
+        const gwdelay = wkTime - gwTime + 5 + spacer;
+        const wkdelay2 = 10 + spacer;
+        //making blocks so we can JSON.stringify() arguments passed to worker scripts
+        const threads = { hack: hThreads, weaken1: wThreads1, grow: gThreads, weaken2: wThreads2 };
+        const delay = { hack: hkdelay, weaken1: wkdelay1, grow: gwdelay, weaken2: wkdelay2 };
+        const scripts = { hack: 'protohack.js', weaken1: 'protoweak.js', grow: 'protogrow.js', weaken2: 'protoweak.js' };
+        const portwrite = { hack: false, weaken1: false, grow: false, weaken2: true };
+
+        for (const job of jobs) {
+            const metrics = { target: values.optimalTarget, job: job, delay: delay[job], port: ns.pid, log: logPort, portwrite: portwrite[job] };
+                    ns.writePort(logPort, 'running ' + scripts[job] + ' at "home" with ' + threads[job] + ' threads and parameter: ' + JSON.stringify(metrics));
+                    ns.exec(scripts[job], 'home', threads[job], JSON.stringify(metrics));
+
+            
         }
 
-        let expectedEndh = performance.now() + hkTime + hkDelay;
-        let expectedStarth = performance.now() + hkDelay;
-        let expectedEndw1 = performance.now() + wkTime + wk1Delay;
-        let expectedEndg = performance.now() + gwTime + gwDelay;
-        let expectedStartg = performance.now() + gwDelay;
-        let expectedEndw2 = performance.now() + wkTime + wk2Delay;
-        let timeRemainingh = expectedEndh - performance.now();
-        let timeToStarth = expectedStarth - performance.now();
-        let timeRemainingw1 = expectedEndw1 - performance.now();
-        let timeRemainingg = expectedEndg - performance.now();
-        let timeToStartg = expectedStartg - performance.now();
-        let timeRemainingw2 = expectedEndw2 + wk2Delay - performance.now();
-        while (timeRemainingw2 > 0) {
-            timeRemainingh = expectedEndh - performance.now();
-            timeRemainingw1 = expectedEndw1 - performance.now();
-            timeRemainingg = expectedEndg - performance.now();
-            timeRemainingw2 = expectedEndw2 + wk2Delay - performance.now();
-            timeToStarth = expectedStarth - performance.now();
-            timeToStartg = expectedStartg - performance.now();
-            ns.clearLog();
-            ns.print('Target: ' + server.hostname);
-            ns.print('Hack amount: ' + greed);
-            ns.print('  Item       :  Hack, 1st Weaken, Grow, 2nd Weaken')
-            ns.print('HWGW threads : ' + hThreads + ', ' + wThreads1 + ', ' + gThreads + ', ' + wThreads2);
-            ns.print('HWGW times   : ' + ns.formatNumber(hkTime, 0, 1000000) + ', ' + ns.formatNumber(wkTime, 0, 1000000) + ', ' + ns.formatNumber(gwTime, 0, 1000000) + ', ' + ns.formatNumber(wkTime, 0, 1000000));
-            ns.print('HWGW delays  : ' + ns.formatNumber(hkDelay, 0, 1000000) + ', ' + ns.formatNumber(wk1Delay, 0, 1000000) + ', ' + ns.formatNumber(gwDelay, 0, 1000000) + ', ' + ns.formatNumber(wk2Delay, 0, 1000000));
-            ns.print('HWGW ends    : ' + ns.formatNumber(hkDelay + hkTime, 0, 1000000) + ', ' + ns.formatNumber(wkTime + wk1Delay, 0, 1000000) + ', ' + ns.formatNumber(gwTime + gwDelay, 0, 1000000) + ', ' + ns.formatNumber(wkTime + wk2Delay, 0, 1000000));
-            ns.print('hackPID: ' + hackPID);
-            if (timeToStarth > 0) {
-                ns.print('hack will initiate process in: ' + ns.tFormat(timeToStarth));
-            } else {
-                ns.print('hack has started.');
-            }
-            ns.print('weakPID1: ' + weak1PID);
-            ns.print('growPID: ' + growPID);
-            if (timeToStartg > 0) {
-                ns.print('grow will initiate process in: ' + ns.tFormat(timeToStartg));
-            } else {
-                ns.print('grow has started.');
-            }
-            ns.print('weakPID2: ' + weak2PID);
-            ns.print('weakens will apply in: ' + ns.tFormat(timeRemainingw2));
-            await ns.sleep(100);
+        ns.clearLog();
+        ns.print('Target: ' + values.optimalTarget);
+        ns.print(`Greed level: ${greed * 100}%`);
+        ns.print('  Item       :  Hack, 1st Weaken, Grow, 2nd Weaken')
+        ns.print('HWGW threads : ' + hThreads + ', ' + wThreads1 + ', ' + gThreads + ', ' + wThreads2);
+        ns.print('HWGW times   : ' + ns.formatNumber(hkTime, 0, 1000000) + ', ' + ns.formatNumber(wkTime, 0, 1000000) + ', ' + ns.formatNumber(gwTime, 0, 1000000) + ', ' + ns.formatNumber(wkTime, 0, 1000000));
+        ns.print(`RAM allocated: ${threads.hack * 1.7 + (threads.weaken1 + threads.weaken2 + threads.grow) * 1.75}/${values.totalThreads * 1.75} GBs`);
+        ns.print(`Expected yield: \$${ns.formatNumber(hackSet * (1000 / (wkTime + 20 + spacer)), 2)} per second`); 
+        ns.print(`Next batch at ${new Date(Date.now() + wkdelay2 + wkTime).toLocaleTimeString(undefined, { hour: "numeric", minute: "numeric", second: "numeric", hour12: true })} (~${ns.tFormat(wkTime + 20 + spacer)})`);
+
+        //values.optimalTarget = getBestTarget(ns, updateTarget(ns)).name; //gets best target
+        values.optimalTarget = updateTarget(ns).sort((a, b) => b.score - a.score)[1].name //second best target
+        server = ns.getServer(values.optimalTarget);
+
+        await dataPort.nextWrite();
+
+        if (!isPrepped(ns, values.optimalTarget)) {
+            await prepTarget(ns, values.optimalTarget);
         }
 
-        ns.print('Countdown Complete.');
-
+        ramNetwork = [];
+        values.minServerRamSize = Infinity;
+        values.maxServerRamSize = 0;
+        values.totalThreads = 0
+        buildramNetwork(ns, ramNetwork, values);
+        ramNetwork.sort((x, y) => x.ram - y.ram);
     }
 } //make sure grow.js, weak.js, and hack.js exist
