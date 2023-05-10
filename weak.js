7004da9e570e5e99c57abaabb850c3bd17cbca35
Sordani <131472756+Sordani@users.noreply.github.com>
2023-05-10 12:31:24 -0700

    Update weak.js
    updated to receive arguments entirely in the form of metrics, including end time, job, and several other parameters included in shotgun and protobatch.

--- weak.js
+++ weak.js
@@ -1,7 +1,19 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	const target = ns.args[0];
-	// ?? checks for null and undefined statments. this syntax will set to 0 if either of those are true.
-	const waitTime = ns.args[1] ?? 0;
-	await ns.weaken(target,{additionalMsec:waitTime});
+	ns.print('metrics: ' + ns.args[0]);
+	const metrics = JSON.parse(ns.args[0]);
+	const delay = metrics.end - metrics.time - Date.now();
+	if (delay < 0) {
+		ns.writePort(metrics.log, `ERROR: ${metrics.job} was ${-delay}ms too late.\n`);
+		return;
+	}
+	await ns.weaken(metrics.target, { additionalMsec: delay });
+	const end = Date.now();
+	if (metrics.batch == null) metrics.batch = metrics.port;
+
+	// Weaken2 reports batch number instead of job.
+	ns.atExit(() => {
+		if (metrics.job === "weaken2") ns.writePort(metrics.port, metrics.batch);
+		ns.writePort(metrics.log, `Batch ${metrics.batch}: ${metrics.job} finished at ${end.toString().slice(-6)}/${Math.round(metrics.end).toString().slice(-6)}\n`);
+	});
 }
