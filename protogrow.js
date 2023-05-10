93d0f1f9e90ccae4bfb3f913cfa8a11dd45dbd38
Sordani <131472756+Sordani@users.noreply.github.com>
2023-05-10 12:34:17 -0700

    Create protogrow.js
    works exclusively with protobatch.js

---
+++ protogrow.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	//metrics is an object consisting of target, delay, job, port, and portwrite values.
+	//we stringify them to send multiple arguements at once to the worker
+	//the worker then parses the arguements to turn them back into an object for access.
+	ns.print('metrics: ' + ns.args[0]);
+	const metrics = JSON.parse(ns.args[0]);
+	await ns.grow(metrics.target, { additionalMsec: metrics.delay });
+
+	
+	ns.atExit(() => {
+		if (metrics.portwrite == true) ns.writePort(metrics.port, metrics.job);
+		try {ns.writePort(metrics.log, 'metrics: ' + ns.args[0]);} catch {}
+	});
+}
