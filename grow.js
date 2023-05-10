5915994530c598402c839951a8718bc63a437f2f
Sordani <131472756+Sordani@users.noreply.github.com>
2023-05-10 12:32:15 -0700

    Update grow.js
    see weak.js

--- grow.js
+++ grow.js
@@ -1,7 +1,13 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	const target = ns.args[0];
-	// ?? checks for null and undefined statments. this syntax will set to 0 if either of those are true.
-	const waitTime = ns.args[1] ?? 0;
-	await ns.grow(target,{additionalMsec:waitTime});
+	ns.print('metrics: ' + ns.args[0]);
+	const metrics = JSON.parse(ns.args[0]);
+	const delay = metrics.end - metrics.time - Date.now();
+	if (delay < 0) {
+		if (metrics.log != null) ns.writePort(metrics.log, `ERROR: ${metrics.job} was ${-delay}ms too late.\n`);
+		return;
+	}
+	await ns.grow(metrics.target, { additionalMsec: delay });
+	const end = Date.now();
+	ns.writePort(metrics.log, `Batch ${metrics.batch}: ${metrics.job} finished at ${end.toString().slice(-6)}/${Math.round(metrics.end).toString().slice(-6)}\n`);
 }
