24975a5f1aa85ca4102880b53013e62ab255d22e
Sordani <131472756+Sordani@users.noreply.github.com>
2023-05-19 13:04:47 -0700

    Update shotgun.js
    removed some sleeps. still figuring out why the 2nd run through it attempts to execute a script with less than 1 thread.

--- shotgun.js
+++ shotgun.js
@@ -15,124 +15,124 @@ export async function main(ns) {
 	ns.atExit(() => ns.kill(logPort));  // Kill the logger when the controller ends.
 	await ns.sleep(1000); // This is just to give the helper a moment to initialize.
 
-	//while (true) {//while loop starts here once we get this shotgun working
-	await getAccess(ns, getServerList(ns));
+	while (true) {//while loop starts here once we get this shotgun working
+		await getAccess(ns, getServerList(ns));
 
-	const ramNetwork = [];
-	const values = {
-		totalThreads: 0,
-		optimalTarget: 'n00dles',
-		maxBlockSize: 0,
-		minBlockSize: Infinity,
-		spacer: 5,
-		buffer: 2000,
-		bestIncome: 0,
-		greed: 0,
-		depth: 0,
-		hThreads: 0,
-		gThreads: 0,
-		homegThreads: 0,
-		wThreads1: 0,
-		homewThreads1: 0,
-		wThreads2: 0,
-		homewThreads2: 0,
-		batchSize: 0,
-		homebatchSize: 0,
-		shots: 0,
-		shells: 0
-	}
-	ns.writePort(logPort, 'building ramNetwork');
-	await ns.sleep(1000);
+		const ramNetwork = [];
+		const values = {
+			totalThreads: 0,
+			optimalTarget: 'n00dles',
+			maxBlockSize: 0,
+			minBlockSize: Infinity,
+			spacer: 5,
+			buffer: 2000,
+			recoil: 100,
+			reload: 100,
+			shots: 0,
+			magazine: 100,
+			bestIncome: 0,
+			greed: 0,
+			depth: 0,
+			hThreads: 0,
+			gThreads: 0,
+			homegThreads: 0,
+			wThreads1: 0,
+			homewThreads1: 0,
+			wThreads2: 0,
+			homewThreads2: 0,
+			batchSize: 0,
+			homebatchSize: 0,
+			shells: 0
+		}
 
-	buildramNetwork(ns, ramNetwork, values);
-	//values.optimalTarget = getBestTarget(ns, updateTarget(ns)); //best target
-	values.optimalTarget = 'joesguns'; //test target
-	ns.writePort(logPort, 'prep-check - line 48 shotgun.js');
-	await ns.sleep(1000);
+		buildramNetwork(ns, ramNetwork, values);
+		values.optimalTarget = getBestTarget(ns, updateTarget(ns)); //best target
+		//values.optimalTarget = 'joesguns'; //test target
+		ns.writePort(logPort, 'prep-check - line 48 shotgun.js');
 
-	if (!isPrepped(ns, values.optimalTarget)) {
-		while (isPrepped(ns, values.optimalTarget) == false) {
-			ns.print("awaiting prepTarget");
+
+		while (!isPrepped(ns, values.optimalTarget)) {
 			await prepTarget(ns, values.optimalTarget);
-			ns.print("should be done with prepTarget");
+			values.optimalTarget = getBestTarget(ns, updateTarget(ns));
 			await ns.sleep(1000);
 		}
-	}
-	await ns.sleep(1000);
-	ns.print('optimizeShotguncall - line 59 shotgun.js');
-	optimizeShotgun(ns, ramNetwork, values);
-	await ns.sleep(1000);
-	const server = ns.getServer(values.optimalTarget);
-	const player = ns.getPlayer();
-	const shells = []; //new array of blocks from ramNetwork that will contain batchspace value
-	ns.print('loadShotgunShells call - line 65 shotgun.js');
-	loadShotgunShells(ns, ramNetwork, values, shells); //function that fills shells
-	shells.sort((x, y) => x.batchSpace - y.batchSpace); //sorts from smallest to largest
-	ns.writePort(logPort, JSON.stringify(values));
-	ns.writePort(logPort, JSON.stringify(shells));
-	await ns.sleep(1000);
 
-	const wTime = ns.getWeakenTime(values.optimalTarget);
-	const hTime = wTime / 4;
-	const gTime = hTime * 3.2;
-	const times = { hack: hTime, weaken1: wTime, grow: gTime, weaken2: wTime };
+		optimizeShotgun(ns, ramNetwork, values);
+		const server = ns.getServer(values.optimalTarget);
+		const player = ns.getPlayer();
+		const shells = []; //new array of blocks from ramNetwork that will contain batchspace value
+		loadShotgunShells(ns, ramNetwork, values, shells); //function that fills shells
+		shells.sort((x, y) => x.batchSpace - y.batchSpace); //sorts from smallest to largest
+		ns.writePort(logPort, JSON.stringify(values));
+		ns.writePort(logPort, JSON.stringify(shells));
 
-	let batchEnd;
-	let batchCounter = 0;
+		const wTime = ns.getWeakenTime(values.optimalTarget);
+		const hTime = wTime / 4;
+		const gTime = hTime * 3.2;
+		const times = { hack: hTime, weaken1: wTime, grow: gTime, weaken2: wTime };
 
+		let batchCounter = 0;
+		let batchEnd;
 
-	while (shells.length > 0) {
-		const jobs = ["weaken2", "grow", "weaken1", "hack"];
-		const offset = values.spacer * batchCounter * 4; // You can see how we'r using the spacer more clearly here.
-		const hEnd = Date.now() + wTime + values.buffer + values.spacer * 1 + offset;
-		const wEnd1 = Date.now() + wTime + values.buffer + values.spacer * 2 + offset;
-		const gEnd = Date.now() + wTime + values.buffer + values.spacer * 3 + offset;
-		const wEnd2 = Date.now() + wTime + values.buffer + values.spacer * 4 + offset;
+		while (shells.length > 0) {
+			const jobs = ["weaken2", "grow", "weaken1", "hack"];
+			const offset = values.spacer * batchCounter * 4; // You can see how we'r using the spacer more clearly here.
+			const hEnd = Date.now() + wTime + values.buffer + values.spacer * 1 + offset;
+			const wEnd1 = Date.now() + wTime + values.buffer + values.spacer * 2 + offset;
+			const gEnd = Date.now() + wTime + values.buffer + values.spacer * 3 + offset;
+			const wEnd2 = Date.now() + wTime + values.buffer + values.spacer * 4 + offset;
 
-		const ends = { hack: hEnd, weaken1: wEnd1, grow: gEnd, weaken2: wEnd2 };
-		const scripts = { hack: "hack.js", weaken1: "weak.js", grow: "grow.js", weaken2: "weak.js" };
-		const threads = { hack: values.hThreads, weaken1: values.wThreads1, grow: values.gThreads, weaken2: values.wThreads2 };
-		const homethreads = { hack: values.hThreads, weaken1: values.homewThreads1, grow: values.homegThreads, weaken2: values.homewThreads2 };
-		ns.print('beginning shell batch for loop.');
-		for (const job of jobs) {
-			const metrics = { batch: batchCounter, target: values.optimalTarget, job: job, time: times[job], end: ends[job], port: ns.pid, log: logPort };
-			ns.scp(scripts[job], shells[0].server);
-			if (shells[0].server == 'home') {
-				ns.exec(scripts[job], shells[0].server, homethreads[job], JSON.stringify(metrics));
-				continue;
+			const ends = { hack: hEnd, weaken1: wEnd1, grow: gEnd, weaken2: wEnd2 };
+			const scripts = { hack: "hack.js", weaken1: "weak.js", grow: "grow.js", weaken2: "weak.js" };
+			const threads = { hack: values.hThreads, weaken1: values.wThreads1, grow: values.gThreads, weaken2: values.wThreads2 };
+			ns.writePort(logPort, JSON.stringify(threads));
+			const homethreads = { hack: values.hThreads, weaken1: values.homewThreads1, grow: values.homegThreads, weaken2: values.homewThreads2 };
+			ns.writePort(logPort, 'beginning shell batch for loop.');
+			for (const job of jobs) {
+				const metrics = { batch: batchCounter, target: values.optimalTarget, job: job, time: times[job], end: ends[job], port: ns.pid, log: logPort };
+				ns.scp(scripts[job], shells[0].server);
+				if (shells[0].server == 'home') {
+					ns.exec(scripts[job], shells[0].server, homethreads[job], JSON.stringify(metrics));
+					continue;
+				}
+				ns.exec(scripts[job], shells[0].server, threads[job], JSON.stringify(metrics));
 			}
-			ns.exec(scripts[job], shells[0].server, threads[job], JSON.stringify(metrics));
+			if (batchCounter++ > (values.depth * 1.2)) {
+				ns.print("ERROR: Infinite loop failsafe triggered.");
+				ns.print('shells: ' + JSON.stringify(shells));
+				ns.print('values: ' + JSON.stringify(values));
+				ns.print('batchCounter: ' + batchCounter);
+				ns.print('wTime: ' + wTime);
+				ns.exit();
+			}
+			if (shells[0].batchSpace-- == 0) {
+				let eShell = shells.shift();
+				ns.print('ejecting shell: ' + eShell);
+				ns.print('shell ejected. loading shot ' + batchCounter);
+
+			}
+			values.shots++
+			if (values.shots == values.reload) {
+				values.reload += values.magazine;
+				await ns.sleep(values.recoil);
+			}
+
+			batchEnd = wEnd2;
 		}
-		if (batchCounter++ > values.depth) {
-			ns.print("ERROR: Infinite loop failsafe triggered.");
-			ns.print('shells: ' + JSON.stringify(shells));
-			ns.print('values: ' + JSON.stringify(values));
-			ns.print('batchCounter: ' + batchCounter);
-			ns.print('this happened inside the while (block.batchSpace > 0) loop');
-			ns.exit();
-		}
-		shells[0].batchSpace--;
-		if (shells[0].batchSpace === 0) {
-			shells.shift;
-			ns.print('shell ejected. loading shot ' + batchCounter);
-		}
 
-	}
 
-	batchEnd = wEnd2;
+		do {
 
-	do {
+			ns.clearLog();
+			ns.print(`Target: ${values.optimalTarget}`);
+			ns.print(`Batches deployed: ${batchCounter}`);
+			ns.print(`Target depth: ${Math.floor(values.depth)}`);
+			ns.print(`Greed level: ${Math.round(values.greed * 1000) / 10}%`);
+			ns.print(`RAM allocated: ${values.hThreads * 1.7 + (values.wThreads1 + values.wThreads2 + values.gThreads) * 1.75 * batchCounter}/${values.totalThreads * 1.75} GBs`);
+			ns.print(`Expected yield: \$${ns.formatNumber(batchCounter * (ns.hackAnalyze(values.optimalTarget) * values.hThreads) * server.moneyMax * (60000 / (values.spacer * 4 * batchCounter + wTime + values.buffer)), 2)} per minute`);
+			ns.print(`Next batch at ${new Date(batchEnd).toLocaleTimeString(undefined, { hour: "numeric", minute: "numeric", second: "numeric", hour12: true })} (~${ns.tFormat(batchEnd - Date.now())})`);
+			await dataPort.nextWrite();
+		} while (dataPort.read() !== batchCounter - 1);
 
-		ns.clearLog();
-		ns.print(`Target: ${values.optimalTarget}`);
-		ns.print(`Batches deployed: ${batchCounter}`);
-		ns.print(`Target depth: ${Math.floor(values.depth)}`);
-		ns.print(`Greed level: ${Math.round(values.greed * 1000) / 10}%`);
-		ns.print(`RAM allocated: ${values.hThreads * 1.7 + (values.wThreads1 + values.wThreads2 + values.grow) * 1.75 * batchCounter}/${values.totalThreads * 1.75} GBs`);
-		ns.print(`Expected yield: \$${ns.formatNumber(batchCounter * (ns.hackAnalyze(values.optimalTarget) * values.hThreads) * server.moneyMax * (60000 / (values.spacer * 4 * batchCounter + wTime + values.buffer)), 2)} per minute`);
-		ns.print(`Next batch at ${new Date(batchEnd).toLocaleTimeString(undefined, { hour: "numeric", minute: "numeric", second: "numeric", hour12: true })} (~${ns.tFormat(batchEnd - Date.now())})`);
-		await dataPort.nextWrite();
-	} while (dataPort.read() !== batchCounter - 1);
-
-	//} //while true wraparound to the whole thing once it's working
+	} //while true wraparound to the whole thing once it's working
 }
