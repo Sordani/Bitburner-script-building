4e0715093483441df80157ab106a945e080e518a
Sordani <131472756+Sordani@users.noreply.github.com>
2023-07-07 17:11:29 -0700

    Create pasiveincome.js
    this script was originally auto-hacknet.js, pirated from the world wide web. no more! we wrote our own hacknet purchasing script. just in time for hacknets to fundamentally change altogether and need additional work to take advantage of additional complexities.

---
+++ pasiveincome.js
@@ -0,0 +1,53 @@
+/** @param {NS} ns */
+export function printStuff(ns) {
+	let production = 0;
+	for (let i = 0; i < ns.hacknet.numNodes(); i++) {
+		const node = ns.hacknet.getNodeStats(i);
+		production += node.production
+	}
+	ns.clearLog();
+	ns.resizeTail(350, 100);
+	ns.moveTail(850, 680);
+	ns.print(ns.hacknet.numNodes() + " out of 23 Hacknet nodes");
+	ns.print("Production: $" + ns.formatNumber(production, 3, 1000, true) + " per sec.");
+}
+
+/** @param {NS} ns */
+export async function main(ns) {
+	ns.disableLog("ALL");
+	ns.tail();
+	while (ns.hacknet.numNodes() === 0) {
+		if (ns.hacknet.getPurchaseNodeCost() < ns.getServerMoneyAvailable("home")) { ns.hacknet.purchaseNode(); }
+		await ns.sleep(0);
+	}
+	while (ns.hacknet.numNodes() > 0) {
+		if (ns.hacknet.numNodes == 23 && ns.hacknet.getNodeStats(22).level == 200 && ns.hacknet.getNodeStats(22).cores == 16 && ns.hacknet.getNodeStats(22).ram == 64) { ns.exit(); }
+		let newNodeCost = ns.hacknet.getPurchaseNodeCost();
+		let printcheck = false;
+		for (let i = 0; i < ns.hacknet.numNodes(); i++) {
+			const node = ns.hacknet.getNodeStats(i);
+			if (ns.hacknet.getLevelUpgradeCost(i) < newNodeCost && node.level < 200 && ns.hacknet.getLevelUpgradeCost(i) < ns.getServerMoneyAvailable("home")) {
+				ns.hacknet.upgradeLevel(i);
+				printcheck = true;
+				await ns.sleep(0);
+			}
+			if (ns.hacknet.getRamUpgradeCost(i) < newNodeCost && ns.hacknet.getRamUpgradeCost(i) < ns.getServerMoneyAvailable("home")) {
+				ns.hacknet.upgradeRam(i);
+				printcheck = true;
+				await ns.sleep(0);
+			}
+			if (ns.hacknet.getCoreUpgradeCost(i) < newNodeCost && ns.hacknet.getCoreUpgradeCost(i) < ns.getServerMoneyAvailable("home")) {
+				ns.hacknet.upgradeCore(i);
+				printcheck = true;
+				await ns.sleep(0);
+			}
+		}
+		if (newNodeCost < ns.getServerMoneyAvailable("home") && ns.hacknet.numNodes() < 23) {
+			ns.hacknet.purchaseNode();
+			printcheck = true;
+		}
+		if (printcheck) { printStuff(ns); }
+		await ns.sleep(0);
+	}
+
+}
